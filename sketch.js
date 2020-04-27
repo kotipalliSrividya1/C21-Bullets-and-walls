0e5fa8bf622cb6b8e492774ff5309c52524ae09d
kotipalliSrividya1 <64408691+kotipalliSrividya1@users.noreply.github.com>
2020-04-27 18:54:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,13 +2,14 @@
 var bullet,wall,weight, speed,thickness;
 function setup() {
   createCanvas(1200,400);
-  bullet=createSprite(100,100,30,30);
   wall=createSprite(1200,200,60,400);
   wall.shapeColor=color(80,80,80);
+
   weight=random(30,52);
   speed=random(223,321);
+ 
+  bullet=new Bullet(weight,speed)
   thickness=random(22,83);
-  bullet.velocityX=15;
 }
 function draw() {
   background("blue");  
@@ -16,22 +17,26 @@ function draw() {
   drawSprites();
 }
 function damage(){
+  bullet.obj.velocityX=0;
  var damage=0.5*(weight*speed*speed)/(thickness*thickness*thickness);
 
  if(damage>10){
   textSize(20);
   text("Not effective Wall",600,200);
-  bullet.shapeColor=color(255,0,0);
+  wall.shapeColor=color(255,0,0);
  }
  if(damage<=10){
   textSize(20);
   text("Good",600,200);
-  bullet.shapeColor=color(0,255,0);
+  wall.shapeColor=color(0,255,0);
  }
 }
-function isTounching(wall,bullet){
-  if(wall.x-bullet.x<wall.width/2+bullet.width/2){
-    bullet.velocityX=0;
+function isTounching(wallA,bulletA){
+
+  if(wallA.x-bulletA.obj.x<wallA.width/2+bulletA.obj.width/2){
+    
     damage();
   }
 }
+
+
